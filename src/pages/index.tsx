ba587752b3b2ab82c89bc1c12d717bafe114bb2f
Hugo Lyra <me@hugolyra.com>
2021-04-25 07:31:23 -0300

    :tada: Init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Sanai Portas</title>
+      </Head>
+      <h1>Sanai Portas</h1>
+    </div>
+  )
+}
