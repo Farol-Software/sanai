56468334fb040985c1d7e2fe53faf534da215efc
Hugo Lyra <me@hugolyra.com>
2022-01-27 17:05:38 +0000

    Add whats to mobile menu

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -21,6 +21,16 @@ const Header = (): JSX.Element => {
           <li>Principal</li>
           <li>Trabalhos</li>
           <li>Blog</li>
+
+          <div className={styles.mobileContact}>
+            <img src="/whats.png" alt="Whatsapp Logo" />
+            <section>
+              <p>Vendas/WhatsApp</p>
+              <a href="https://wa.me/5581996572406">
+                <strong>(81) 9 9657-2406</strong>
+              </a>
+            </section>
+          </div>
         </ul>
 
         <div className={styles.contact}>
